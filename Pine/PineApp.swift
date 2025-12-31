73ab24d7764079762587806a6314ea3237d01a1a
Bing-Chang Lai <johnny.lai@me.com>
2025-12-31 15:53:04 -0800

    Empty

---
+++ Pine/PineApp.swift
@@ -0,0 +1,32 @@
+//
+//  PineApp.swift
+//  Pine
+//
+//  Created by Bing-Chang Lai on 12/31/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PineApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PineTests/PineTests.swift
@@ -0,0 +1,17 @@
+//
+//  PineTests.swift
+//  PineTests
+//
+//  Created by Bing-Chang Lai on 12/31/25.
+//
+
+import Testing
+@testable import Pine
+
+struct PineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
